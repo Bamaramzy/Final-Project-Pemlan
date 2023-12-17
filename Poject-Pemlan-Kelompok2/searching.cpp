508fbc1e41c1b185d0f4a39ec5338a147b01d82d
Erlangga Putra Ramadhan <51524196+elangrama22@users.noreply.github.com>
2023-12-18 01:44:36 +0700

    change to sequential search

--- Poject-Pemlan-Kelompok2/searching.cpp
+++ Poject-Pemlan-Kelompok2/searching.cpp
@@ -1,3 +1,9 @@
-#include <iostream>
-
-using namespace std;
+int searchJudulPeminjam(Peminjam peminjam[], int size, string search){
+    int i = 0;
+    while (i != size){
+        if (peminjam[i].judulBuku == search){
+            return i;
+        }
+        i++;
+    }
+}
