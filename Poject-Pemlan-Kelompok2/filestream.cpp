ca77d0eaf74df4b6890c9a6db0809d261df55c0d
Bamaramzy <144866519+Bamaramzy@users.noreply.github.com>
2023-12-17 21:54:40 +0700

    Update filestream.cpp

--- Poject-Pemlan-Kelompok2/filestream.cpp
+++ Poject-Pemlan-Kelompok2/filestream.cpp
@@ -1,49 +1,122 @@
-// library.cpp
+// main.cpp
 
-#include "library.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
+
 using namespace std;
 
-void Library::displayAllBooks() {
-    ifstream file("database_buku.txt");
-    string line;
+struct Buku
+{
+    string judulBuku;
+    string penulis;
+    int tahunTerbit;
+    int jumlah;
+};
 
-    cout << "Judul_Buku\tPenulis\tTahun_Terbit\n";
-    cout << "--------------------------------\n";
+struct Peminjam
+{
+    string namaPeminjam;
+    string judulBuku;
+};
 
-    while (getline(file, line)) {
-        cout << line << endl;
+class Library
+{
+public:
+    void displayAllBooks(Buku buku[], int size);
+    void displayAllPeminjam(Peminjam peminjam[], int size);
+    void sortByTitle(Buku buku[], int size);
+    void readBooksFromFile(Buku buku[], int &size, const string &filename);
+    void readPeminjamFromFile(Peminjam peminjam[], int &size, const string &filename);
+};
+
+void Library::displayAllBooks(Buku buku[], int size)
+{
+    for (int i = 0; i < size; ++i)
+    {
+        cout << buku[i].judulBuku << " " << buku[i].penulis << " " << buku[i].tahunTerbit << " " << buku[i].jumlah << endl;
     }
-
-    file.close();
 }
 
-void Library::displayAllPeminjam() {
-    ifstream file("database_peminjam.txt");
-    string line;
-
-    cout << "Nama_Peminjam\tJudul_Buku\n";
-    cout << "--------------------------\n";
-
-    while (getline(file, line)) {
-        cout << line << endl;
+void Library::displayAllPeminjam(Peminjam peminjam[], int size)
+{
+    for (int i = 0; i < size; ++i)
+    {
+        cout << peminjam[i].namaPeminjam << " " << peminjam[i].judulBuku << endl;
     }
-
-    file.close();
 }
 
-void Library::borrowBook() {
-    string borrower, bookTitle;
+void Library::sortByTitle(Buku buku[], int size)
+{
+    sort(buku, buku + size, [](const Buku &a, const Buku &b)
+         { return a.judulBuku < b.judulBuku; });
+}
 
-    cout << "Masukkan Nama Peminjam: ";
-    cin.ignore();
-    getline(cin, borrower);
+void Library::readBooksFromFile(Buku buku[], int &size, const string &filename)
+{
+    ifstream infile(filename);
+    if (!infile)
+    {
+        cerr << "File " << filename << " tidak dapat dibuka.\n";
+        return;
+    }
 
-    cout << "Masukkan Judul Buku yang ingin dipinjam: ";
-    getline(cin, bookTitle);
+    size = 0;
+    while (infile >> buku[size].judulBuku >> buku[size].penulis >> buku[size].tahunTerbit >> buku[size].jumlah)
+    {
+        ++size;
+        if (size >= 20) // Memastikan tidak melebihi kapasitas array
+            break;
+    }
 
-    ofstream peminjamFile("database_peminjam.txt", ios::app);
-    peminjamFile << borrower << '\t' << bookTitle << '\n';
-    peminjamFile.close();
+    infile.close();
+}
 
-    cout << "Buku '" << bookTitle << "' berhasil dipinjam oleh " << borrower << ".\n";
+void Library::readPeminjamFromFile(Peminjam peminjam[], int &size, const string &filename)
+{
+    ifstream infile(filename);
+    if (!infile)
+    {
+        cerr << "File " << filename << " tidak dapat dibuka.\n";
+        return;
+    }
+
+    size = 0;
+    while (infile >> peminjam[size].namaPeminjam >> peminjam[size].judulBuku)
+    {
+        ++size;
+        if (size >= 20) // Memastikan tidak melebihi kapasitas array
+            break;
+    }
+
+    infile.close();
+}
+
+int main()
+{
+    const int maxSize = 20;
+    Buku buku[maxSize];
+    Peminjam peminjam[maxSize];
+
+    int sizeBuku = 0;
+    int sizePeminjam = 0;
+
+    Library library;
+
+    // Membaca data buku dari file
+    library.readBooksFromFile(buku, sizeBuku, "dbBuku.txt");
+
+    // Membaca data peminjam dari file
+    library.readPeminjamFromFile(peminjam, sizePeminjam, "dbPeminjam.txt");
+
+    // Menampilkan data buku
+    cout << "Data Buku:\n";
+    library.displayAllBooks(buku, sizeBuku);
+
+    // Menampilkan data peminjam
+    cout << "\nData Peminjam:\n";
+    library.displayAllPeminjam(peminjam, sizePeminjam);
+
+    return 0;
 }
