ce7b40a9586dba368581b05c759163be8b644fe0
Bamaramzy <144866519+Bamaramzy@users.noreply.github.com>
2023-12-17 21:32:40 +0700

    Create library.h

---
+++ Poject-Pemlan-Kelompok2/library.h
@@ -0,0 +1,17 @@
+// library.h
+
+#ifndef LIBRARY_H
+#define LIBRARY_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+class Library {
+public:
+    void displayAllBooks();
+    void displayAllPeminjam();
+    void borrowBook();
+};
+
+#endif // LIBRARY_H
