f1787c02c34a6169db63d5015260ae22217949f3
Erlangga Putra Ramadhan <51524196+elangrama22@users.noreply.github.com>
2023-12-18 22:02:04 +0700

    add replace space<>underscore

--- Poject-Pemlan-Kelompok2/string.cpp
+++ Poject-Pemlan-Kelompok2/string.cpp
@@ -1,11 +1,54 @@
 bool panjangnama(string nama){
     int panjang = nama.size();
     if (panjang < 4){
-        cout<<"Nama terlalu pendek";
+        cout<<"Nama terlalu pendek\n";
+        system("pause");
         return false;
     }
     else if (panjang > 50){
-        cout<<"Nama terlalu panjang";
+        cout<<"Nama terlalu panjang\n";
+        system("pause");
+        return false;
+    }
+    else {
         return true;
     }
+}
+
+string replace2Underscore(string& S) {
+    string s1, s2;
+    char c1 = ' ';
+    char c2 = '_';
+    for (char c : S) {
+        if (c == c1) {
+            s1 += c2;
+            s2 += c1;
+        } else if (c == c2) {
+            s1 += c1;
+            s2 += c2;
+        } else {
+            s1 += c;
+            s2 += c;
+        }
+    }
+    return s1;
+}
+
+string replace2Space(string& S) {
+    string s1, s2;
+    char c1 = '_';
+    char c2 = ' ';
+    for (char c : S) {
+        if (c == c1) {
+            s1 += c2;
+            s2 += c1;
+        } else if (c == c2) {
+            s1 += c1;
+            s2 += c2;
+        } else {
+            s1 += c;
+            s2 += c;
+        }
+    }
+    return s1;
 }
